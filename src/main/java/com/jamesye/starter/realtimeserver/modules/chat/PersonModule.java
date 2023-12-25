8a5d0e8dfe93d92805e088c21c5bdee02a3af0fa
LuC4s Bérgamo <bergamo86@gmail.com>
2023-12-24 23:15:44 -0300

    person module

---
+++ src/main/java/com/jamesye/starter/realtimeserver/modules/chat/PersonModule.java
@@ -0,0 +1,53 @@
+package com.jamesye.starter.realtimeserver.modules.chat;
+
+import com.corundumstudio.socketio.HandshakeData;
+import com.corundumstudio.socketio.SocketIONamespace;
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.listener.ConnectListener;
+import com.corundumstudio.socketio.listener.DataListener;
+import com.corundumstudio.socketio.listener.DisconnectListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonModule {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonModule.class);
+
+    private final SocketIONamespace namespace;
+
+    private  int count = 0;
+
+    @Autowired
+    public PersonModule(SocketIOServer server) {
+        this.namespace = server.addNamespace("/person");
+        this.namespace.addConnectListener(onConnected());
+        this.namespace.addDisconnectListener(onDisconnected());
+        this.namespace.addEventListener("person", ChatMessage.class, onChatReceived());
+    }
+
+    private DataListener<ChatMessage> onChatReceived() {
+        return (client, data, ackSender) ->
+        {
+            count++;
+            log.debug(count +" - Client[{}] - Received person message '{}'", client.getSessionId().toString(), data);
+            namespace.getBroadcastOperations().sendEvent("chat", data);
+        };
+    }
+
+    private ConnectListener onConnected() {
+        return client -> {
+            HandshakeData handshakeData = client.getHandshakeData();
+            log.debug("Client[{}] - Connected to person module through '{}'", client.getSessionId().toString(), handshakeData.getUrl());
+        };
+    }
+
+    private DisconnectListener onDisconnected() {
+        return client -> {
+            log.debug("Client[{}] - Disconnected from person module.", client.getSessionId().toString());
+        };
+    }
+
+}
